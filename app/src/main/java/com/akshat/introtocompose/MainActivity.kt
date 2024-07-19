e347407a4d5afb99adc4a2ff3dfdce4936817976
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-07-19 13:12:04 -0400

    Money Counter simple app where used Modifiers, Layouts surface, box and column, Clickable events, States, State Hoisting

--- app/src/main/java/com/akshat/introtocompose/MainActivity.kt
+++ app/src/main/java/com/akshat/introtocompose/MainActivity.kt
@@ -1,47 +1,121 @@
 package com.akshat.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.akshat.introtocompose.ui.theme.IntroToComposeTheme
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+      //  enableEdgeToEdge()
         setContent {
             IntroToComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(){
+    val moneyCounter = remember {
+        mutableStateOf(0)
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$${moneyCounter.value}", style = TextStyle(
+                color = Color.White,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
+            ))
+            Spacer(modifier = Modifier.height(100.dp))
+            createCircle(moneyCounter = moneyCounter.value){ newValue ->
+                moneyCounter.value = newValue
+            }
+
+            if (moneyCounter.value > 25){
+                Text(text = "lots of money !!")
+            }
+        }
+
+    }
 }
+
+//@Preview
+@Composable
+fun createCircle(moneyCounter: Int = 0,
+                 updateMoneyCounter: (Int) -> Unit){
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp),
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(4.dp),
+        onClick = {
+            updateMoneyCounter(moneyCounter + 1)
+        }
+    ) {
+        Box(modifier =
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier)
+        }
+
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
